6cd081d53ea3388ba714594e032440c2d882240f
김예찬 <ycraah@gamil.com>
2024-10-13 09:20:46 +0900

    mock 데이터 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {useEffect, useState} from 'react'
 import './App.css'
 import Header from './component/Header'
 import TodoEditor from './component/TodoEditor'
 import TodoList from './component/TodoList'
 
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    createdDate: new Date().getTime()
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래 넣기',
+    createdDate: new Date().getTime()
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    createdDate: new Date().getTime()
+  }
+]
+
 function App() {
+  const [todo, setTodo] = useState([mockTodo])
   return (
     <div className="App">
       <Header />
