9bc6c4aca2e1c770f28b7b4cd30ecc9da94e2aac
김예찬 <ycraah@gamil.com>
2024-10-19 10:48:36 +0900

    테일윈드 css 적용 위치 변경, 날짜 한글로 출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import './App.css'
 import Header from './component/Header'
 
 function App() {
   return (
-    <div className="w-[500px] h-[500px] border-2 mx-auto">
+    <div className="w-[500px] h-[500px] border-2 border-gray-500 mx-auto mt-5 p-5">
       <Header />
       <h1>TodoEditor Component</h1>
       <h1>TodoList Component</h1>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,18 @@
 function Header() {
-  return <h1>Header import Complete</h1>
+  const today = new Date()
+  return (
+    <div className="mb-5">
+      <h1 className="text-3xl font-bold">오늘은 📅</h1>
+      <h1 className="mt-5 text-3xl font-bold text-gray-400">
+        {new Date().toLocaleDateString('ko-KR', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          weekday: 'long'
+        })}
+      </h1>
+    </div>
+  )
 }
 
 export default Header
